d191aea91fc1a13d7eafe85da87583b6d3eff3d3
Abdul Rafique Sultan <sultanabdulrafique@gmail.com>
2025-09-21 06:36:23 -1200

    added Price Table Layout
    A simple minimalistic price table layout

---
+++ UI COMPONENTS/PRICING TABLE LAYOUT/main.js
@@ -0,0 +1,38 @@
+// Functionality
+
+function premium() {
+    document.getElementById("n1").style.display = "flex";
+    document.getElementById("n2").style.display = "flex";
+    document.getElementById("n3").style.display = "flex";
+    document.getElementById("n4").style.display = "flex";
+
+
+    document.getElementById("n5").style.display = "flex";
+    document.getElementById("n6").style.display = "flex";
+    document.getElementById("n7").style.display = "flex";
+    document.getElementById("n8").style.display = "flex";
+}
+
+function pro() {
+    document.getElementById("n1").style.display = "flex";
+    document.getElementById("n2").style.display = "flex";
+    document.getElementById("n3").style.display = "flex";
+    document.getElementById("n4").style.display = "flex";
+
+
+    document.getElementById("n5").style.display = "none";
+    document.getElementById("n6").style.display = "none";
+    document.getElementById("n7").style.display = "none";
+    document.getElementById("n8").style.display = "none";
+}
+
+function starter() {
+    document.getElementById("n1").style.display = "none";
+    document.getElementById("n2").style.display = "none";
+    document.getElementById("n3").style.display = "none";
+    document.getElementById("n4").style.display = "none";
+    document.getElementById("n5").style.display = "none";
+    document.getElementById("n6").style.display = "none";
+    document.getElementById("n7").style.display = "none";
+    document.getElementById("n8").style.display = "none";
+}
